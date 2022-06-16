0e0f0df8cea80559d033ba8433c4f67a243c8a97
Minh Lys <lyminh1994@gmail.com>
2022-06-17 00:35:49 +0700

    Move to typescript

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test("renders coming soon tex", () => {
-  render(<App />);
-  const h1Element = screen.getByText(/Wah Gwan, Soon Come/i);
-  expect(h1Element).toBeInTheDocument();
-});
